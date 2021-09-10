414493fa83de52c2b9ee6d121499c1cdf1482f15
Vinod Patel <v_p88@hotmail.com>
2021-09-10 17:28:29 +0100

    updated products table

--- backend/data/products.sql
+++ backend/data/products.sql
@@ -1,5 +1,30 @@
 insert into product (name, description, price, stock_count, image_url)
-VALUES ('signature, shirt', 'mens white oxford style shirt with black buttons', 35, 35, '../images/shirt.jpg');
+VALUES ('Signature, shirt', 'mens white oxford style shirt with black buttons', 35, 40, '../images/shirt1.jpg');
+
+insert into product (name, description, price, stock_count, image_url)
+VALUES ('Sky - relaxed, shirt', 'sky blue loose fitted shirt. 100% polyester. Perfect for summer casual wear', 38, 27, '../images/shirt2.jpg');
+
+insert into product (name, description, price, stock_count, image_url)
+VALUES ('Pin Stripe - office}, shirt', 'Formal shirt - white with black pinstripes. 85% polyester. 15% elastane.', 45, 3, '../images/shirt3.jpg');
+
+insert into product (name, description, price, stock_count, image_url)
+VALUES ('White - Peach stripes, shirt', 'Formal shirt - white with peach stripes, 100% polyester', 43, 81, '../images/shirt4.jpg');
+
+insert into product (name, description, price, stock_count, image_url)
+VALUES ('Black - smart', 'Smart black shirt - fitted, 100% polyester', 40, 32, '../images/shirt5.jpg');
+
+insert into product (name, description, price, stock_count, image_url)
+VALUES ('White - smart', 'Smart white shirt - fitted, 100% polyester', 40, 34, '../images/shirt6.jpg');
+
+insert into product (name, description, price, stock_count, image_url)
+VALUES ('Workwear - checkered - red', 'Outdoor workwear shirt, red and blue checkered. ', 50, 23, '../images/shirt7.jpg');
+
+insert into product (name, description, price, stock_count, image_url)
+VALUES ('Office - blue', 'Smart shirt perfect for the office of for casual. 100% polyester', 36, 28, '../images/shirt8.jpg');
+
+
+
+
 
 
 
